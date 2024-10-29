69f075f698f8d0e55c60f687c48281cceba5c3a3
angellsla <1509518916@qq.com>
2024-10-29 17:36:23 +0800

    妈的index.js忘记丢修改之后的了

--- js/index.js
+++ js/index.js
@@ -2,24 +2,21 @@
 var musicList = [];
 // 声明变量，保存当前播放的是哪一首歌曲
 var currentIndex = 0;
-// 实现列表切换功能所用的列表信息
-var musicLists = [
-  { name: '默认', url: 'https://raw.githubusercontent.com/angellsla/music-player/main/music.json' },
-  { name: '《遮羞艾莉》', url: 'https://raw.githubusercontent.com/angellsla/music-player/main/zxal.json' },
-  { name: 'liet2', url: 'https://raw.githubusercontent.com/angellsla/music-player/main/list2.json' },
-];
-
 
 // 加载音乐列表信息
-function loadMusicList(url) {
+function loadMusicList(listName) {
   $.ajax({
     type: "GET",
-    url: url,
+    url: "loadMusic.php?listName=" + encodeURIComponent(listName),
     dataType: "json",
     success: function(data) {
       musicList = data;
-      render(musicList[currentIndex]);
-      renderMusicList(musicList);
+      if (musicList.length > 0) {
+        currentIndex = 0;
+        render(musicList[currentIndex]);
+        renderMusicList(musicList);
+        $("audio").get(0).load(); // 加载音频
+      }
     },
     error: function(jqXHR, textStatus, errorThrown) {
       console.log("Error loading the music list: " + textStatus + ", " + errorThrown);
@@ -27,51 +24,124 @@ function loadMusicList(url) {
   });
 }
 
+// 填充 select 元素
+function populateMusicSelect() {
+  var select = $('#musicSelect');
+  select.empty(); // 清空现有选项
+  select.append($('<option></option>').val('default').html('默认'));
+  // 其他列表选项可以在这里添加
+  select.val('default'); // 默认选择第一个列表
+}
+
+// 给 select 元素绑定 change 事件
+$('#musicSelect').on('change', function() {
+  var selectedListName = $(this).val();
+  loadMusicList(selectedListName); // 根据选择的列表名称加载音乐列表
+});
+
+// 页面加载完成后，填充 select 元素并加载默认音乐列表
+$(document).ready(function() {
+  populateMusicSelect();
+  loadMusicList('default'); // 默认加载全部音乐列表
+});
+
+// 根据信息，设置页面对应标签中的内容
+function render(data) {
+  $(".name").text(data.name);
+  $(".singer-album").text(`${data.singer} - ${data.album}`);
+  $(".time").text(data.time);
+  $(".cover img").attr("src", data.cover);
+  $("audio").attr("src", data.audio_url);
+  $(".mask_bg").css({
+    background: `url("${data.cover}") no-repeat center center`
+  });
+  $("audio").get(0).load(); // 重置音频元素
+}
+
+// 根据音乐列表数据，创建li
+function renderMusicList(list) {
+  $(".music-list").empty();
+  list.forEach(function(item, index) {
+    var $li = $("<li>")
+      .addClass(index === currentIndex ? "playing" : "")
+      .append($("<span>").text(`${index + 1}. ${item.name} - ${item.singer}`))
+      .append($("<span>")
+        .addClass("fa")
+        .addClass(index === currentIndex && $("audio").get(0).paused ? "fa-pause-circle" : "fa-play-circle")
+        .attr("data-index", index)
+        .attr("aria-hidden", true));
+    $(".music-list").append($li);
+  });
+}
+
+// 格式化时间
+function formatTime(time) {
+  var min = parseInt(time / 60, 10);
+  var sec = parseInt(time % 60, 10);
+  min = min < 10 ? "0" + min : min;
+  sec = sec < 10 ? "0" + sec : sec;
+  return `${min}:${sec}`;
+}
+
+// 监听audio标签的 timeupdate 事件
+$("audio").on("timeupdate", function () {
+  var audio = $("audio").get(0);
+  var currentTime = audio.currentTime || 0;
+  var duration = audio.duration || 0;
+  $(".current-time").text(formatTime(currentTime));
+  var value = (currentTime / duration) * 100;
+  $(".music_progress_line").css({
+    width: value + "%"
+  });
+});
+
+// 监听音乐播放完毕的事件
+$("audio").on("ended", function () {
+  $("#playBtn").removeClass("fa-pause").addClass("fa-play");
+  $(".cover").css({
+    "animation-play-state": "paused"
+  });
+  if (currentIndex < musicList.length - 1) {
+    currentIndex++;
+  } else {
+    currentIndex = 0;
+  }
+  render(musicList[currentIndex]);
+  $("#playBtn").trigger("click");
+});
+
+// 通过事件委托给音乐列表的播放按钮绑定点击事件
+$(".music-list").on("click", ".play-circle", function () {
+  var index = $(this).attr("data-index");
+  currentIndex = parseInt(index, 10);
+  render(musicList[currentIndex]);
+  $("#playBtn").trigger("click");
+});
+
 // 给播放按钮绑定点击事件
 $("#playBtn").on("click", function () {
-  if ($("audio").get(0).paused) {
-    // 暂停状态，应该播放
-    // 修改播放按钮的图标状态
+  var audio = $("audio").get(0);
+  if (audio.paused) {
     $(this).removeClass("fa-play").addClass("fa-pause");
-    // 让音乐信息卡片显示出来
-    $(".player-info").animate(
-      {
-        top: "-120%",
-        opacity: 1,
-      },
-      "slow"
-    );
-
-    // 让封面旋转起来
+    $(".player-info").animate({
+      top: "-120%",
+      opacity: 1
+    }, "slow");
     $(".cover").css({
-      "animation-play-state": "running",
+      "animation-play-state": "running"
     });
-
-    // 让音乐播放起来
-    $("audio").get(0).play();
+    audio.play();
   } else {
-    // 播放状态，应该暂停
     $(this).removeClass("fa-pause").addClass("fa-play");
-    // 让音乐信息卡片消失
-    $(".player-info").animate(
-      {
-        top: "0%",
-        opacity: 0,
-      },
-      "slow"
-    );
-
-    // 让封面旋转暂停
+    $(".player-info").animate({
+      top: "0%",
+      opacity: 0
+    }, "slow");
     $(".cover").css({
-      "animation-play-state": "paused",
+      "animation-play-state": "paused"
     });
-
-    // 让音乐暂停
-    $("audio").get(0).pause();
+    audio.pause();
   }
-
-  // 重新渲染列表数据
-  renderMusicList(musicList);
 });
 
 // 给上一首按钮绑定点击事件
@@ -81,10 +151,7 @@ $("#prevBtn").on("click", function () {
   } else {
     currentIndex = musicList.length - 1;
   }
-
-  // 重新渲染歌曲信息
   render(musicList[currentIndex]);
-  // 让音乐播放
   $("#playBtn").trigger("click");
 });
 
@@ -95,130 +162,16 @@ $("#nextBtn").on("click", function () {
   } else {
     currentIndex = 0;
   }
-  // 重新渲染歌曲信息
   render(musicList[currentIndex]);
-  // 让音乐播放
   $("#playBtn").trigger("click");
 });
 
-// 给音乐列表绑定点击事件
-$("#openModal").on("click", function () {
-  $(".modal").css({
-    display: "block",
-  });
-});
-
-$(".modal-close").on("click", function () {
-  $(".modal").css({
-    display: "none",
-  });
-});
-
-// 监听audio标签的 timeupdate 事件
-$("audio").on("timeupdate", function () {
-  // 获取音乐当前到的时间，单位：秒
-  var currentTime = $("audio").get(0).currentTime || 0;
-  // 获取音乐的总时长，单位：秒
-  var duration = $("audio").get(0).duration || 0;
-  // 设置当前播放时间
-  $(".current-time").text(formatTime(currentTime));
-  // 设置进度条
-  var value = (currentTime / duration) * 100;
-  $(".music_progress_line").css({
-    width: value + "%",
-  });
-});
-
-// 监听音乐播放完毕的事件
-$("audio").on("ended", function () {
-  $("#playBtn").removeClass("fa-pause").addClass("fa-play");
-  // 让封面旋转暂停
-  $(".cover").css({
-    "animation-play-state": "paused",
-  });
-});
-
-// 通过事件委托给音乐列表的播放按钮绑定点击事件
-$(".music-list").on("click", ".play-circle", function () {
-  if ($(this).hasClass("fa-play-circle")) {
-    var index = $(this).attr("data-index");
-    currentIndex = index;
-    render(musicList[currentIndex]);
-    $("#playBtn").trigger("click");
-  } else {
-    $("#playBtn").trigger("click");
-  }
-});
-
-// 格式化时间
-function formatTime(time) {
-  // 329 -> 05:29
-  var min = parseInt(time / 60);
-  var sec = parseInt(time % 60);
-  min = min < 10 ? "0" + min : min;
-  sec = sec < 10 ? "0" + sec : sec;
-
-  return `${min}:${sec}`;
-}
-
-// 根据信息，设置页面对应标签中的内容
-function render(data) {
-  $(".name").text(data.name);
-  $(".singer-album").text(`${data.singer} - ${data.album}`);
-  $(".time").text(data.time);
-  $(".cover img").attr("src", data.cover);
-  $("audio").attr("src", data.audio_url);
-  $(".mask_bg").css({
-    background: `url("${data.cover}") no-repeat center center`,
-  });
-}
-
-// 根据音乐列表数据，创建li
-function renderMusicList(list) {
-  $(".music-list").empty();
-
-  $.each(list, function (index, item) {
-    var $li = $(`
-      <li class="${index == currentIndex ? "playing" : ""}">
-        <span>0${index + 1}. ${item.name} - ${item.singer}</span>
-        <span data-index="${index}" class="fa ${
-      index == currentIndex && !$("audio").get(0).paused
-        ? "fa-pause-circle"
-        : "fa-play-circle"
-    } play-circle"></span>
-      </li>
-    `);
-    $(".music-list").append($li);
-  });
-}
-
-// 填充 select 元素
-function populateMusicSelect() {
-  var select = $('#musicSelect');
-  select.empty(); // 清空现有选项
-  $.each(musicLists, function(index, list) {
-    select.append($('<option></option>').val(list.url).html(list.name));
-  });
-  select.val(musicLists[0].url); // 默认选择第一个列表
-}
-
-// 给 select 元素绑定 change 事件
-$('#musicSelect').on('change', function() {
-  var selectedUrl = $(this).val();
-  loadMusicList(selectedUrl);
-});
-
-// 页面加载完成后，填充 select 元素
-$(document).ready(function() {
-  populateMusicSelect();
-  loadMusicList(musicLists[0].url); // 默认加载第一个列表
-});
-
+// 监听音量控制滑块变化
 document.addEventListener('DOMContentLoaded', function() {
   var audioPlayer = document.getElementById('audio');
   var volumeControl = document.getElementById('volumeControl');
-  audioPlayer.volume = volumeControl.value;  // 设置初始音量
+  audioPlayer.volume = volumeControl.value; // 设置初始音量
   volumeControl.addEventListener('input', function() {
-      audioPlayer.volume = this.value;
-  });  // 监听滑条变化事件
+    audioPlayer.volume = this.value; // 监听滑条变化事件
+  });
 });
